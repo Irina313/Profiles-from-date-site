46afd5d9a0142c270fd42a163b09039ea7be1e16
Irina <irina1989_ru@ukr.net>
2025-01-16 22:19:21 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import DatingProfile from './DatingProfile';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <DatingProfile img = "https://unsplash.com/photos/liy0P6AmGPM" name = "Bob" age = "32 years old" description = "Looking for smart, funny and easy going girl"/>
+    <DatingProfile img = "https://unsplash.com/photos/RM3XFaZuwpE" name = "Steve" age = "35 years old" description = "Looking for intelligent and well-mannered woman" />
+    <DatingProfile img = "https://unsplash.com/photos/liy0P6AmGP" name = "James" age = "35 years old" description = "Looking for sporty and active girl who llikes rock music" />
+    </div>
+  );
+}
+
+export default App;
